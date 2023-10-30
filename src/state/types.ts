c7e0a85be7f8c37fcace56f4b183b0e9c2150e24
therealmanraj <singhmanraj8@gmail.com>
2023-10-30 22:38:15 +0530

    First row visualisations

---
+++ src/state/types.ts
@@ -0,0 +1,33 @@
+export interface GetKpisResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData: Array<Day>;
+}
+
+export interface ExpensesByCategory {
+  salaries: number;
+  supplies: number;
+  services: number;
+}
+
+export interface Month {
+  id: string;
+  month: string;
+  revenue: number;
+  expenses: number;
+  nonOperationalExpenses: string;
+  operationalExpenses: string;
+}
+
+export interface Day {
+  id: string;
+  month: string;
+  revenue: number;
+  expenses: number;
+}
